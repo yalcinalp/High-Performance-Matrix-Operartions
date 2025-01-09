8a30f5897a4ce7a47b032fada973f34feec14cc0
Alp Eren Yalcin <95969634+yalcinalp@users.noreply.github.com>
2025-01-10 01:44:55 +0300

    Update kernels.c

--- kernels.c
+++ kernels.c
@@ -5,13 +5,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "defs.h"
-/*
- * Please fill in the following team_t struct
- */
+
 team_t team = {
 
-        "eXXXXXX",      /* First student ID */
-        "NAME",       /* First student name */
+        "eXXXXXX",      
+        "Alp",       
 
 };
 
@@ -19,10 +17,6 @@ team_t team = {
 /********************
  * NORMALIZATION KERNEL
  ********************/
-
-/****************************************************************
- * Your different versions of the normalization functions go here
- ***************************************************************/
 
 /*
  * naive_normalize - The naive baseline version of convolution
@@ -52,23 +46,46 @@ void naive_normalize(int dim, float *src, float *dst) {
     }
 }
 
-/*
- * normalize - Your current working version of normalization
- * IMPORTANT: This is the version you will be graded on
- */
+
 char normalize_descr[] = "Normalize: Current working version";
-void normalize(int dim, float *src, float *dst)
-{
-    naive_normalize(dim,src,dst);
+void normalize(int dim, float *src, float *dst) {
+    if (dim <= 0) return;
+
+    float min = src[0];
+    float max = src[0];
+    
+    int i;
+    int l = dim * dim;
+    int unrol = 4;
+    
+    for (i = 0; i < l; i += unrol) {
+        float val1 = src[i];
+        float val2 = src[i+1];
+        float val3 = src[i+2];
+        float val4 = src[i+3];
+        
+        min = val1 < min ? val1 : min;
+        min = val2 < min ? val2 : min;
+        min = val3 < min ? val3 : min;
+        min = val4 < min ? val4 : min;
+        
+        max = val1 > max ? val1 : max;
+        max = val2 > max ? val2 : max;
+        max = val3 > max ? val3 : max;
+        max = val4 > max ? val4 : max;
+    }
+
+    float scale = 1.0f / (max - min);
+    
+    for (i = 0; i < l; i += unrol) {
+        dst[i] = (src[i] - min) * scale;
+        dst[i+1] = (src[i+1] - min) * scale;
+        dst[i+2] = (src[i+2] - min) * scale;
+        dst[i+3] = (src[i+3] - min) * scale;
+    }
+    
 }
 
-/*********************************************************************
- * register_normalize_functions - Register all of your different versions
- *     of the normalization functions  with the driver by calling the
- *     add_normalize_function() for each test function. When you run the
- *     driver program, it will test and report the performance of each
- *     registered test function.
- *********************************************************************/
 
 void register_normalize_functions() {
     add_normalize_function(&naive_normalize, naive_normalize_descr);
@@ -82,10 +99,6 @@ void register_normalize_functions() {
 /************************
  * KRONECKER PRODUCT KERNEL
  ************************/
-
-/********************************************************************
- * Your different versions of the kronecker product functions go here
- *******************************************************************/
 
 /*
  * naive_kronecker_product - The naive baseline version of k-hop neighbours
@@ -103,25 +116,35 @@ void naive_kronecker_product(int dim1, int dim2, float *mat1, float *mat2, float
     }
 }
 
-
-
-/*
- * kronecker_product - Your current working version of kronecker_product
- * IMPORTANT: This is the version you will be graded on
- */
 char kronecker_product_descr[] = "Kronecker Product: Current working version";
 void kronecker_product(int dim1, int dim2, float *mat1, float *mat2, float *prod)
 {
-    naive_kronecker_product(dim1,dim2,mat1,mat2,prod);
-}
+    int i, j, k, l;
 
-/******************************************************************************
- * register_kronecker_product_functions - Register all of your different versions
- *     of the kronecker_product with the driver by calling the
- *     add_kronecker_product_function() for each test function. When you run the
- *     driver program, it will test and report the performance of each
- *     registered test function.  
- ******************************************************************************/
+    for (i = 0; i < dim1; i++) {
+    
+    	int c = 0;
+        for (j = 0; j < dim1; j++) {
+            float scalar = mat1[RIDX(i, j, dim1)];
+
+            for (k = 0; k < dim2; k++) {
+                int    r = (i * dim2 + k) * (dim1 * dim2);
+                
+                float *mat2_ptr  = &mat2[RIDX(k, 0, dim2)];
+                float *prod_ptr = &prod[r + c];
+
+                for (l = 0; l < dim2; l += 4) {
+                    prod_ptr[l] = scalar * mat2_ptr[l];
+                    prod_ptr[l + 1] = scalar * mat2_ptr[l + 1];
+                    prod_ptr[l + 2] = scalar * mat2_ptr[l + 2];
+                    prod_ptr[l + 3] = scalar * mat2_ptr[l + 3];
+                }
+            }
+            
+            c += dim2;
+        }
+    }
+}
 
 void register_kronecker_product_functions() {
     add_kronecker_product_function(&naive_kronecker_product, naive_kronecker_product_descr);
